456c79c5552b7586bc7a2cc7930549383a0d08af
MMenninger <menninger@gmail>
2015-10-18 11:12:26 -0500

    started to write code to bring in data

--- DownloadCTA.R
+++ DownloadCTA.R
@@ -5,21 +5,34 @@ dir.create("Zips")
 dir.create("Unzips")
 
 #Download One File
-x <-1
-webdata[x,1]
-webPath<- webdata[x,1]
-target<- file.path(PCPath, paste("Zips/", webdata[x,3], webdata[x,4], webdata[x,5], sep=""))
-download.file(webdata[x,1], target)
-unzip(target, exdir = file.path("Unzips/", paste(webdata[x,3], "_", webdata[x,4], "_", substr(webdata[x,5], 1, 4), sep="")))
+i <-1
+webdata[i,1]
+folder <- paste(webdata[i,3], "_", webdata[i,4], "_", substr(webdata[i,5], 1, 4), sep="")
+target<- file.path(PCPath, paste("Zips/", folder, ".zip", sep= ""))
+download.file(webdata[i,1], target)
+unzip(target, exdir = file.path("Unzips/", folder))
      
 
 #Download All Files
 for (i in 1:nrow(webdata)) {
-                            webdata[i,1]
-                            webPath<- webdata[i,1]
-                            target<- file.path(PCPath, paste("Zips/", webdata[i,3], webdata[i,4], webdata[i,5], sep=""))
+                            folder <- paste(webdata[i,3], "_", webdata[i,4], "_", substr(webdata[i,5], 1, 4), sep="")
+                            folder #tells you what file is working on
+                            target<- file.path(PCPath, paste("Zips/", folder, ".zip", sep=""))
                             download.file(webdata[i,1], target)
-                            unzip(target, exdir = file.path(PCPath, paste("Unzips/", webdata[i,3], "_", webdata[i,4], "_", substr(webdata[i,5], 1, 4), sep="")))
+                            unzip(target, exdir = file.path(PCPath, paste("Unzips/", folder, sep="")))
                             }
 
+#Load FIles in R
+#for (i in 1:nrow(webdata)) {
+for (i in 1:1) {
+                folder <- paste(webdata[i,3], "_", webdata[i,4], "_", substr(webdata[i,5], 1, 4), sep="")
+                folder #tells you what file working on
+                Active <- file.path(getwd(), "Unzips", folder)
+  
+                }
 
+list.files("Unzips")
+
+list.files(Active)
+agency<-read.csv(paste(Active, "/agency.txt", sep=""))
+agency<-rbind(read.csv(paste(Active, "/agency.txt", sep="")), agency)
