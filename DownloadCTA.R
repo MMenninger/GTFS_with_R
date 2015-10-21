6ae6708f2e8ff4977e9b7c4463a274ecf5a53a88
MMenninger <menninger@gmail.com>
2015-10-21 09:53:43 -0500

    simplify by removing elseif, creating data frames in advance.

--- DownloadCTA.R
+++ DownloadCTA.R
@@ -26,50 +26,38 @@ for (i in 1:nrow(webdata)) {
 #Clean up
 rm(list = c("agency", "calendar", "calendar_dates", "frequencies", "routes", "shapes", "stop_times", "stops", "transfers", "trips"))
 
+agency <-data.frame() #May be a better way to make all of these??
+calendar <-data.frame()
+calendar_dates<-data.frame()
+frequencies<-data.frame()
+routes<-data.frame()
+shapes<-data.frame()
+stop_times<-data.frame()
+stops<-data.frame()
+transfers<-data.frame()
+trips<-data.frame()
+
 #Load Files in R
-for (i in 1:nrow(webdata)) {
-#for (i in 1:1) {
+#for (i in 1:nrow(webdata)) {
+for (i in 1:1) {
                 folder <- paste(webdata[i,3], "_", webdata[i,4], "_", substr(webdata[i,5], 1, 4), sep="")
                 folder #tells you what file working on
                 Active <- file.path(getwd(), "Unzips", folder)
+                
+                list.files(Active) [i] #For testing
+                file_path_sans_ext(list.files(Active) [i]) # For testing
+                
+                agency<-read.csv(paste(Active, "/agency.txt", sep=""))
+                calendar<-read.csv(paste(Active, "/calendar.txt", sep=""))
+                calendar_dates<-read.csv(paste(Active, "/calendar_dates.txt", sep=""))
+                frequencies<-read.csv(paste(Active, "/frequencies.txt", sep=""))
+                routes<-read.csv(paste(Active, "/routes.txt", sep=""))
+                shapes<-read.csv(paste(Active, "/shapes.txt", sep=""))
+                stop_times<-read.csv(paste(Active, "/stop_times.txt", sep=""))
+                stops<-read.csv(paste(Active, "/stops.txt", sep=""))
+                transfers<-read.csv(paste(Active, "/transfers.txt", sep=""))
+                trips<-read.csv(paste(Active, "/trips.txt", sep=""))
+               }
 
-                ifelse(exists("agency"), 
-                       (agency<-rbind.fill(read.csv(paste(Active, "/agency.txt", sep="")), agency)),
-                       (agency<-read.csv(paste(Active, "/agency.txt", sep=""))))
-
-                ifelse(exists("calendar"), 
-                       (calendar<-rbind.fill(read.csv(paste(Active, "/calendar.txt", sep="")), calendar)),
-                       (calendar<-read.csv(paste(Active, "/calendar.txt", sep=""))))
-                
-                ifelse(exists("calendar_dates"), 
-                       (calendar_dates<-rbind.fill(read.csv(paste(Active, "/calendar_dates.txt", sep="")), calendar_dates)),
-                       (calendar_dates<-read.csv(paste(Active, "/calendar_dates.txt", sep=""))))
-                
-                ifelse(exists("frequencies"), 
-                       (frequencies<-rbind.fill(read.csv(paste(Active, "/frequencies.txt", sep="")), frequencies)),
-                       (frequencies<-read.csv(paste(Active, "/frequencies.txt", sep=""))))
-                
-                ifelse(exists("routes"), 
-                       (routes<-rbind.fill(read.csv(paste(Active, "/routes.txt", sep="")), routes)),
-                       (routes<-read.csv(paste(Active, "/routes.txt", sep=""))))
-                
-                ifelse(exists("shapes"), 
-                       (shapes<-rbind.fill(read.csv(paste(Active, "/shapes.txt", sep="")), shapes)),
-                       (shapes<-read.csv(paste(Active, "/shapes.txt", sep=""))))
-                
-                ifelse(exists("stop_times"), 
-                       (stop_times<-rbind.fill(read.csv(paste(Active, "/stop_times.txt", sep="")), stop_times)),
-                       (stop_times<-read.csv(paste(Active, "/stop_times.txt", sep=""))))
-                
-                ifelse(exists("stops"), 
-                       (stops<-rbind.fill(read.csv(paste(Active, "/stops.txt", sep="")), stops)),
-                       (stops<-read.csv(paste(Active, "/stops.txt", sep=""))))
-                
-                ifelse(exists("transfers"), 
-                       (transfers<-rbind.fill(read.csv(paste(Active, "/transfers.txt", sep="")), transfers)),
-                       (transfers<-read.csv(paste(Active, "/transfers.txt", sep=""))))
-                
-                ifelse(exists("trips"), 
-                       (trips<-rbind.fill(read.csv(paste(Active, "/trips.txt", sep="")), trips)),
-                       (trips<-read.csv(paste(Active, "/trips.txt", sep=""))))
-                }
+shapes<-unique(shapes)
+stop_times<-unique(stop_times)
