e90bd5ab9ca42be7398345a4c4430848dffc42f2
Martin Menninger <menninger@gmail.com>
2015-10-23 13:48:36 -0500

    structure from database

--- DownloadCTA.R
+++ DownloadCTA.R
@@ -27,20 +27,21 @@ for (i in 1:nrow(webdata)) {
 #Clean up
 rm(list = c("agency", "calendar", "calendar_dates", "frequencies", "routes", "shapes", "stop_times", "stops", "transfers", "trips"))
 
-agency <-data.frame() #May be a better way to make all of these??
-calendar <-data.frame()
-calendar_dates<-data.frame()
-frequencies<-data.frame()
-routes<-data.frame()
-shapes<-data.frame()
-stop_times<-data.frame()
-stops<-data.frame()
-transfers<-data.frame()
-trips<-data.frame()
+
+agency <-dbReadTable(con,"gtfs_agency") #May be a better way to make all of these??
+calendar <-dbReadTable(con,"gtfs_calendar")
+calendar_dates <-dbReadTable(con,"gtfs_calendar_dates")
+frequencies<-dbReadTable(con,"gtfs_frequencies")
+routes<-dbReadTable(con,"gtfs_routes")
+shapes<-dbReadTable(con,"gtfs_shapes")
+stop_times<-dbReadTable(con,"gtfs_stop_times")
+stops<-dbReadTable(con,"gtfs_stops")
+transfers<-dbReadTable(con,"gtfs_transfers")
+trips<-dbReadTable(con,"gtfs_trips")
 
 #Load Files in R
-for (i in 1:nrow(webdata)) {
-#for (i in 9:10) {
+#for (i in 1:nrow(webdata)) {
+for (i in 9:10) {
                 folder <- paste(webdata[i,3], "_", webdata[i,4], "_", substr(webdata[i,5], 1, 4), sep="")
                 folder #tells you what file working on
                 Active <- file.path(getwd(), "Unzips", folder)
